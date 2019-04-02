12907d3b4094de077d413bbd4e6d01e0c5b3b7a8
Victor Rono <v.rono@icloud.com>
2019-04-02 17:58:32 -0500

    problem #1 solved

---
+++ LINQProblems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Using LINQ, write a function that returns all words that contain the substring “th” from a list
+            //use.Contain() to find "th", create new list, print results
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var newList = words.Where(m => m.Contains("th"));
+            foreach (var el in newList)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadLine();
+        }
+    }
+}
